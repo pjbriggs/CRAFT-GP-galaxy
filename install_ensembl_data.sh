df41ef96f6db26e665be5699e4b124d398d9e52c
Peter Briggs <peter.briggs@manchester.ac.uk>
2016-07-08 22:13:04 +0100

    Add installer for GRCh38 core cache data from Ensembl for VEP.

---
+++ install_ensembl_data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+#
+# Install the reference data for CRAFT-GP for testing from command line
+#
+if [ -z "$CRAFT_GP_DATA" ] ; then
+    echo CRAFT_GP_DATA: not set >&2
+    exit 1
+fi
+if [ ! -d ${CRAFT_GP_DATA}/ensembl/cache/homo_sapiens ] ; then
+    if [ ! -f homo_sapiens_vep_84_GRCh38.tar.gz ] ; then
+	echo Downloading GRCh38 core cache
+	wget -q ftp://ftp.ensembl.org/pub/release-84/variation/VEP/homo_sapiens_vep_84_GRCh38.tar.gz
+    fi
+    echo Unpacking GRCh38 core cache
+    tar zvf homo_sapiens_vep_84_GRCh38.tar.gz -C ${CRAFT_GP_DATA}/ensembl/cache
+fi
+##
+#
