160be3f170adeba0a8ba6d119144169be25905b5
Peter Briggs <peter.briggs@manchester.ac.uk>
2016-09-30 16:33:18 +0100

    Update to source data installer for Roadmap_Epigenomics_15_state_model.

--- install_source_data.sh
+++ install_source_data.sh
@@ -31,7 +31,9 @@ function Roadmap_Epigenomics_15_state_model() {
     tar -zxvf all.dense.browserFiles.tgz
     cd -
     # files are renamed and tabix indexed for use with VEP
-    python $CRAFT_GP_SCRIPTS/process_roadmap.py --state 15 --meta $1/source_data/roadmap_r9/meta_data/roadmap_consolidated_epigenome_ids.csv --rename EDACC
+    cd $1
+    python $CRAFT_GP_SCRIPTS/process_roadmap.py --state 15 --meta source_data/roadmap_r9/meta_data/roadmap_consolidated_epigenome_ids.csv --rename EDACC
+    cd -
 }
 function HapMap_recombination_map() {
     echo "Installing HapMap_recombination_map"
